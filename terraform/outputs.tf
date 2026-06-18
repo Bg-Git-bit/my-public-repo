ef1fed5e507581a73f38fa7abc992b93fd98aa42
Bg-Git-bit <sakshidec02@gmail.com>
2026-06-18 16:21:27 +0530

    zxdszsf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,11 +10,6 @@ output "glue_job_arn" {
   value       = aws_glue_job.data_transformation.arn
 }
 
-output "cloudwatch_log_group" {
-  description = "CloudWatch log group for Glue job"
-  value       = aws_cloudwatch_log_group.glue_logs.name
-}
-
 output "script_location" {
   description = "S3 location of Glue script"
   value       = "s3://${var.s3_output_bucket}/scripts/aws_glue_Script_1.py"
